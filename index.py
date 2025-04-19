1f8d998899e9c2f968b50ff511e9106ef30ee4d2
404BySam <devbysam404@gmail.com>
2025-04-19 10:26:45 +0200

    type() / Concaténation and with f-string

--- index.py
+++ index.py
@@ -28,3 +28,29 @@ notes = ["Bonjour", 18,["Salut", "Super !", [1, 2, 3]], 20, 14, [0, 5]]
 
 print(notes[2][0])
 
+x = 4
+
+print(x)
+print(type(x))
+
+prenom = "Jean"
+nom = "Dupont"
+
+age = 26
+
+taille = 1.75
+
+est_developpeyeur = True
+
+print(type(prenom))
+print(type(nom))
+print(type(age))
+print(type(taille))
+print(type(est_developpeyeur))
+
+# Concaténation
+print("Je m'appelle " + prenom + " et j'ai " + str(age) + " ans.")
+
+# Concaténation avec f-string
+print(f"Je m'appelle {prenom} et j'ai {age} ans. je mesure {taille} m.")
+print(f"est_developpeyeur : {est_developpeyeur}" )
